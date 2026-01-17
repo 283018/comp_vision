19679e7304fb02be954f2d113ccb11927ed7b7b4
283018 <283018@student.pwr.edu.pl>
2026-01-18 00:19:42 +0100

    updated visual expection test (lmao)

--- scripts/_test_augment.py
+++ scripts/_test_augment.py
@@ -1,4 +1,5 @@
 import sys
+from operator import index
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -9,71 +10,53 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from config import cfg
 from data.augmentation import augment, make_lr_hr_pair
-from data.collector import load_image
+from data.collector import RunMode, build_dataset, image_iterator, load_image
 
 
-def show_before_after(image_path):
-    img = load_image(Path(image_path))
-    lr, hr = make_lr_hr_pair(img)
-
-    #!
-    lr_aug, hr_aug = augment(
-        lr,
-        hr,
-        lr_flip_prob = 0,
-        ud_flip_prob = 0.,
-        rot90_prob = 0.,
+def show_before_after():
+    patches_per_img = 4
+    ds, _ = build_dataset(
+        run_mode=RunMode.TRAIN,
+        batch_size=8,
+        patches_per_image=patches_per_img,
+        seed=42,
     )
-    lr_aug = np.clip(lr_aug, 0.0, 1.0)
-    hr_aug = np.clip(hr_aug, 0.0, 1.0)
+    lr_batch, hr_batch = next(iter(ds))
 
-    lr, hr = lr.numpy(), hr.numpy()
-    lr = np.clip(lr, 0.0, 1.0)
-    hr = np.clip(hr, 0.0, 1.0)
+    lr_batch = lr_batch.numpy()
+    hr_batch = hr_batch.numpy()
 
-    fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+    lr_batch = np.clip(lr_batch, 0.0, 1.0)
+    hr_batch = np.clip(hr_batch, 0.0, 1.0)
 
-    axes[0, 0].imshow(lr)
-    axes[0, 0].set_title("LR (before)")
-    axes[0, 1].imshow(hr)
-    axes[0, 1].set_title("HR (before)")
+    fig, axes = plt.subplots(patches_per_img, 2, figsize=(6, 3 * patches_per_img))
 
-    axes[1, 0].imshow(lr_aug)
-    axes[1, 0].set_title("LR (after)")
-    axes[1, 1].imshow(hr_aug)
-    axes[1, 1].set_title("HR (after)")
+    #! shows already augmented images
+    for i in range(patches_per_img):
+        axes[i, 0].imshow(lr_batch[i])
+        axes[i, 0].set_title("LR (augmented)")
 
-    for ax in axes.flat:
-        ax.axis("off")
+        axes[i, 1].imshow(hr_batch[i])
+        axes[i, 1].set_title("HR (augmented)")
+
+        for ax in axes[i]:
+            ax.axis("off")
 
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
-    files = [
-        "div2k/test/0014.png",
-        "div2k/test/0018.png",
-        "div2k/test/0021.png",
-        "div2k/test/0055.png",
-        "div2k/test/0089.png",
-        "div2k/test/0128.png",
-        "div2k/test/0158.png",
-        "div2k/test/0191.png",
-        "div2k/test/0261.png",
-        "div2k/test/0305.png",
-        "div2k/test/0335.png",
-        "div2k/test/0398.png",
-        "div2k/test/0443.png",
-        "div2k/test/0519.png",
-        "div2k/test/0616.png",
-        "div2k/test/0688.png",
-        "div2k/test/0820.png",
-        "div2k/test/0897.png",
-        "div2k/test/0492.png",
-        "div2k/test/0859.png",
-    ]
+    files = image_iterator(
+        RunMode.TEST,
+        (0.9999, 0.0001),
+        data_dir=cfg.DATA_DIR,
+        index_path=cfg.DATA_DIR / "index.json",
+        seed=123321,
+        regenerate_index=False,
+    )
+    files = list(files)
 
     for f in files:
         print(f"{cfg.DATA_DIR / f}")
-        show_before_after(cfg.DATA_DIR / f)
+        show_before_after()
